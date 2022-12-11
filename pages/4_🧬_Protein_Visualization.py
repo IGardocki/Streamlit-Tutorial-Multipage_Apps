ec1dfb8b53825372e56dd64432ea754c8904433e
IGardocki <ian.d.gardocki@gmail.com>
2022-12-11 16:57:44 -0600

    removed cols on protein page

--- pages/4_🧬_Protein_Visualization.py
+++ pages/4_🧬_Protein_Visualization.py
@@ -10,19 +10,19 @@ st.write(
     """This demo illustrates protein visualization using the stmol component (https://github.com/napoles-uach/stmol). """
 )
 
-col1, col2 = st.columns(2)
+# col1, col2 = st.columns(2)
 
-with col1:
-    st.write('This is the structure of thrombin inhibited by AERUGINOSIN298-A from a BLUE-GREEN ALGA. The PDB ID is 1A2C.')
+# with col1:
+st.write('This is the structure of thrombin inhibited by AERUGINOSIN298-A from a BLUE-GREEN ALGA. The PDB ID is 1A2C.')
 
-    with st.empty():
-        xyzview = py3Dmol.view(query='pdb:1A2C') 
-        xyzview.setStyle({'cartoon':{'color':'spectrum'}})
-        showmol(xyzview, height = 500,width=800)
+    # with st.empty():
+xyzview = py3Dmol.view(query='pdb:1A2C') 
+xyzview.setStyle({'cartoon':{'color':'spectrum'}})
+showmol(xyzview, height = 500,width=800)
 
-with col2:
-    st.write(
+# with col2:
+st.write(
     """You can label residues of your choice in the protein, like so."""
-    )
+)
 
-    showmol(render_pdb_resn(viewer = render_pdb(id = '1A2C'),resn_lst = ['ALA', 'ARG']))
+showmol(render_pdb_resn(viewer = render_pdb(id = '1A2C'),resn_lst = ['ALA', 'ARG']))
